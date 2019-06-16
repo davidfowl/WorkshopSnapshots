fa6e0954c8db514a115008b90e2a355e138ba532
David Fowler <davidfowl@gmail.com>
2019-06-16 05:43:57 -0700

    Added everything except for personal agenda

--- FrontEnd/Startup.cs
+++ FrontEnd/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FrontEnd.Middleware;
 using FrontEnd.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -67,6 +68,8 @@ namespace FrontEnd
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseMiddleware<RequireLoginMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
